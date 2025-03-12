c1f94635b19a75e30f1da4c8a5004121730a74db
Matheus Rodrigues <matheus.rodriguesvieira@outlook.com>
2025-03-11 22:49:39 -0300

    Criação/conexão DB

---
+++ PedidosSQLite/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using System.IO;
+using PedidosSQLite.model;
+using SQLitePCL;
+
+
+
+public class Program
+
+{
+    public static void Main()
+    {
+        SQLitePCL.Batteries.Init();
+
+        
+        Console.WriteLine("----- Conexão DB -----\n1) Primeira Conexão\n2) Testar conexão\n3) Sair");
+        string opc = Console.ReadLine();
+        bool loop = true;
+        while (loop)
+        {
+            switch (opc)
+            {
+                case "1":
+                    dbConexao.dbInicializar();
+                    break;
+                case "2":
+                    if (dbConexao.dbConectar())
+                    {
+                        Console.WriteLine("Conexão bem-sucedida");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Falha na conexão");
+                    }
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+        
+
+    }
+}
---
+++ PedidosSQLite/model/dbConexao.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.IO; // Adicionado como alternativa para gerenciar o caminho até o arquivo do DB
+
+namespace PedidosSQLite.model
+{
+    public static class dbConexao
+    {
+        public static void dbInicializar() //Valida se o arquivo do DB existe, caso não exista, cria o arquivo e a tabela
+        {
+            string pastaLocal = AppDomain.CurrentDomain.BaseDirectory; // Obtem a pasta base do projeto
+            string dbCaminho = Path.Combine(pastaLocal, "pedidos.db"); // Cria o caminho até o arquivo do DB
+
+            if (!File.Exists(dbCaminho))
+            {
+                using (FileStream fs = File.Create(dbCaminho))
+                {
+                    Console.WriteLine("Banco de dados criado com sucesso");   // O arquivo é criado aqui
+                }
+            }
+            else
+            {
+                Console.WriteLine("Banco de dados já existe");
+            }
+
+            using (var db = new SqliteConnection($"Filename={dbCaminho}"))
+            {
+                db.Open();
+
+                string criarTabela = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                var createTable = new SqliteCommand(criarTabela, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static bool dbConectar()
+        {
+            string pastaLocal = AppDomain.CurrentDomain.BaseDirectory; // Obtem a pasta base do projeto
+            string dbCaminho = Path.Combine(pastaLocal, "pedidos.db"); // Cria o caminho até o arquivo do DB
+
+            try
+            {
+                using (var db = new SqliteConnection($"Filename={dbCaminho}"))
+                {
+                    db.Open();
+                    return true; // Conexão bem-sucedida
+                }
+            }
+            catch (Exception)
+            {
+                return false; // Falha na conexão
+            }
+        }
+    }
+}
